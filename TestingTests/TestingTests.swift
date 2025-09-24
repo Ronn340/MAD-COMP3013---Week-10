fa348dfe5b8192f15b896c5b2f8d63bdb700ebaa
Ron <ronravnr@gmail.com>
2025-09-24 20:43:53 +1000

    Initial Commit

---
+++ TestingTests/TestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTests.swift
+//  TestingTests
+//
+//  Created by Ron Ramos on 24/9/2025.
+//
+
+import Testing
+@testable import Testing
+
+struct TestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
