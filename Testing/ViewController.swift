72877f7c42d85db0fd17fa7c42e2903ff929e817
Ronn340 <ronravnr@gmail.com>
2025-10-03 12:00:34 +1000

    Added the tableview with values and logo

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let units : [String] = ["Mobile Apps Development", "Computer Graphics", "Technologies for Web Development", "Object Oriented Analysis", "Programming Techniques", "Computer Networking"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return units.count
+    }
+    
+    // assign the values in your array variable to cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel!.text = units[indexPath.row]
+        //cell.imageView???
+    
+        let image:UIImage = UIImage(named: "DevLogo.png" )!
+        cell.imageView?.image = image
+                                    
+        return cell;
+    }
 
 }
 
